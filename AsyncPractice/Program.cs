7b8400a52167635d68add15b4fd83632440c1b1b
hjk7 <hjk7@sfu.ca>
2017-09-05 00:02:04 -0700

    first commit

---
+++ AsyncPractice/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            TaskWaitDemo.Instance.SimpleDelay();
+
+            Console.WriteLine("***Program Ended***");
+            Console.ReadLine();
+
+        }
+
+
+
+    }
+}
---
+++ AsyncPractice/TaskWaitDemo.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncPractice
+{
+    public class TaskWaitDemo
+    {
+        private static TaskWaitDemo instance;
+
+        public TaskWaitDemo(){}
+
+        public static TaskWaitDemo Instance {
+            get
+            {
+                if(instance == null)
+                {
+                    instance = new TaskWaitDemo();
+                }
+                return instance;
+            }
+        }
+
+
+        public void SimpleDelay()
+        {
+            Task taskA = new Task(() => {
+                Thread.Sleep(2000);
+                Console.WriteLine("Hello from taskA");
+            });
+            taskA.Start();
+
+            //taskA.Wait(); //uncommenting this will print the line from taskA first.
+
+            Console.WriteLine("Print me first");
+
+            taskA.Wait();
+        }
+
+
+        public void WaitAll()
+        {
+            Task taskA = new Task(() => {
+                Thread.Sleep(2000);
+                Console.WriteLine("Hello from taskA");
+            });
+            taskA.Start();
+
+            //taskA.Wait(); //uncommenting this will print the line from taskA first.
+
+            Console.WriteLine("Print me first");
+
+            taskA.Wait();
+        }
+
+
+
+    }
+}
